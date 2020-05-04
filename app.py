07861903f5bbcd4f5c320d3b63c88f0d1df79c4a
Vileblood <david.thomas1986@gmail.com>
2020-05-04 18:17:10 +0000

    added mongodb connection with pymongo

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config('MONGO_DBNAME') = recipe_manager
+app.config('MONGO_URI') = mongodb+srv://dthomas86:r00tuser@myfirstcluster-pf6q6.mongodb.net/recipe_manager?retryWrites=true&w=majority
+
 @app.route('/')
 def hello():
     return "Hello World"
