7db7f8822a4d968b96a99b92b7c0e2e1523ef8f3
Vileblood <david.thomas1986@gmail.com>
2020-05-10 19:09:18 +0000

    added connections between form and list of recipes

--- app.py
+++ app.py
@@ -21,6 +21,13 @@ def add_recipe():
     categories = mongo.db.categories.find()
     return render_template('add_recipe.html', recipes=recipes, categories=categories)
 
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes= mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return(redirect(url_for('get_recipes'))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
